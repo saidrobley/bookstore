d0bc207c6b8c9ffd49e9a44718e0a4d16211ab8b
saidrobley <saidrobley@gmail.com>
2021-11-26 15:38:33 -0700

    change style for product page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar/Navbar';
@@ -58,6 +58,9 @@ const Product = () => {
               <Add onClick={() => handleQuantity('inc')} />
             </AmountContainer>
             <Button onClick={handleClick}>ADD TO CART</Button>
+            <Link to="/cart">
+              <Button to="/cart">Check Out</Button>
+            </Link>
           </AddContainer>
         </InfoContainer>
       </Wrapper>
@@ -72,6 +75,7 @@ const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+
   @media (max-width: 768px) {
     flex-direction: column;
   }
@@ -83,16 +87,22 @@ const ImgContainer = styled.div`
 
 const Image = styled.img`
   width: 100%;
-  height: 90vh;
+  height: 70vh;
   object-fit: cover;
 `;
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 50px;
+  display: flex;
+  flex-direction: column;
+  padding-right: 2rem;
+  height: 60vh;
 `;
 
 const Title = styled.h1`
   font-weight: 200;
+  margin: 0;
+  padding: 0;
 `;
 
 const Desc = styled.p`
@@ -107,13 +117,13 @@ const Price = styled.span`
 const AddContainer = styled.div`
   width: 50%;
   display: flex;
-  align-items; center;
-  justify-content: space-between;
+  flex-direction: column;
 `;
 const AmountContainer = styled.div`
   display: flex;
   align-items: center;
   font-weight: 700;
+  margin-top: 20px;
 `;
 
 const Amount = styled.span`
@@ -133,6 +143,8 @@ const Button = styled.button`
   background-color: white;
   cursor: pointer;
   font-weight: 500;
+  width: 50%;
+  margin-top: 2rem;
   &:hover {
     background-color: #f8f4f4;
   }
